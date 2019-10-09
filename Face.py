a76ccb208c5bbc399ff5bc0132881789399e78f4
Tecbioo <antony.lorenzelli&@etu.univ-lorraine.fr>
2019-10-09 11:01:31 +0200

    Ajout de face.py

---
+++ Face.py
@@ -0,0 +1,17 @@
+class Face:
+
+    def _init_(self):
+        self.color = -1
+        self.type = ""
+
+    def _get_color(self):
+        return self.color
+
+    def _set_color(self, v):
+        self.color = v
+
+    def _get_type(self):
+        return type
+
+    def _set_type(self, v):
+        self.type = v
